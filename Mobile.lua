57acc0bac78e55cf52fe7d722b606e0ff22e681f
lethanhdatyeuem <153050799+2Xhub@users.noreply.github.com>
2025-01-01 21:42:02 +0700

    Add files via upload

--- Mobile.lua
+++ Mobile.lua
@@ -3,8 +3,8 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 --------------------------------------------------------------------------------------------------------------------------------------------
 local Window = Fluent:CreateWindow({
-    Title = "Misc  Hub | Blox Fruits V2",
-    SubTitle = "by Đạt Mods",
+    Title = "RaftK Hub",
+    SubTitle = "Blox Fruits",
     TabWidth = 180,
     Size = UDim2.fromOffset(470, 330),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
